dab9940db3f1391042ee38510fa35a6d9d3b7b39
kjbattery <kembarjayabattery20@gmail.com>
2024-11-02 22:03:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // Script bisa digunakan untuk fitur tambahan, jika perlu
 console.log("Selamat datang di Kembar Jaya Battery!");
+
+// Animasi saat di-scroll
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) { // Aktifkan animasi saat scroll lebih dari 50px
+        header.classList.add("scrolled-header");
+    } else {
+        header.classList.remove("scrolled-header");
+    }
+});
